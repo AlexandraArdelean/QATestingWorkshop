68989ffb8145ed48db00342d62b756fc8d5481a1
AlexandraArdelean <ale.xandra91@yahoo.com>
2023-06-12 23:12:56 +0300

    [assertions]

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,18 +1,65 @@
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 public class LoginTest {
-    public void loginWithValidCredentials(){
+    private WebDriver driver;
+
+    @Before
+    public void init() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+
+    @Test
+    public void loginWithValidCredentials() {
         driver.get("http://testfasttrackit.info/selenium-test/");
         driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
         driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
         driver.findElement(By.cssSelector("#email")).sendKeys("test@testing.com");
         driver.findElement(By.cssSelector("#pass")).sendKeys("Testing123");
         driver.findElement(By.cssSelector("#send2 > span > span")).click();
+        WebElement welcomeText = driver.findElement(By.cssSelector(".hello strong"));
+        Assert.assertEquals("Hello, Test T Test!", welcomeText.getText());
+        Assert.assertTrue(welcomeText.isDisplayed());
 
-         driver.quit();
+    }
+
+    @Test
+    public void loginWithInvalidPassword() {
+        driver.get("http://testfasttrackit.info/selenium-test/");
+        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
+        driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
+        driver.findElement(By.cssSelector("#email")).sendKeys("test@testing.com");
+        driver.findElement(By.cssSelector("#pass")).sendKeys("test123");
+        driver.findElement(By.cssSelector("#send2 > span > span")).click();
+        WebElement errorTextElement = driver.findElement(By.cssSelector(".error-msg span"));
+        Assert.assertEquals("Invalid login or password.", errorTextElement.getText());
+
+
+    }
+
+    @Test
+    public void loginWithoutMandatoryFields() {
+        driver.get("http://testfasttrackit.info/selenium-test/");
+        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
+        driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
+        driver.findElement(By.cssSelector("#send2 > span > span")).click();
+        WebElement emailErrorMessage = driver.findElement(By.id("advice-required-entry-email"));
+        Assert.assertEquals("This is a required field.", emailErrorMessage.getText());
+        WebElement passwordErrorMessage = driver.findElement(By.id("advice-required-entry-pass"));
+        Assert.assertEquals("This is a required field.", passwordErrorMessage.getText());
+
+    }
+
+
+    @After
+    public void close() {
+        driver.quit();
     }
 }
